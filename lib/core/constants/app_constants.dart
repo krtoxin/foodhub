e73f4879375fec7b18908ef0a9909a365afbcd45
krtoxin <kimeybaa@gmail.com>
2026-05-29 17:31:40 +0200

    chore: add project structure, l10n files and constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,21 @@
+class AppConstants {
+  static const String mealDbBaseUrl = 'https://www.themealdb.com/api/json/v1/1';
+  static const String mealDbCategoriesEndpoint = '/categories.php';
+  static const String mealDbSearchEndpoint = '/search.php';
+  static const String mealDbFilterEndpoint = '/filter.php';
+  static const String mealDbLookupEndpoint = '/lookup.php';
+  static const String mealDbRandomEndpoint = '/random.php';
+
+  static const String prefTheme = 'theme_mode';
+  static const String prefLanguage = 'language';
+  static const String prefLastSearch = 'last_search';
+  static const String prefRandomMealId = 'random_meal_id';
+  static const String prefRandomMealDate = 'random_meal_date';
+
+  static const String firestoreFavorites = 'favorites';
+  static const String firestoreMyRecipes = 'my_recipes';
+  static const String firestoreUsers = 'users';
+
+  static const String storageProfilePhotos = 'profile_photos';
+  static const String storageRecipePhotos = 'recipe_photos';
+}
