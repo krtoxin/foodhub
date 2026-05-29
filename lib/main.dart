2420bf4e8a4a5c29bc26c503a0edf6fa6abda9f1
krtoxin <kimeybaa@gmail.com>
2026-05-29 18:37:11 +0200

    initialize Firebase and update main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
 import 'core/providers/shared_preferences_provider.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final prefs = await SharedPreferences.getInstance();
 
   runApp(
